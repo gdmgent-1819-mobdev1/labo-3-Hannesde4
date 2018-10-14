d98bb986239e7dff99a1fce305614ff0ca349c36
Hannesde4 <hannes_de_baere@hotmail.com>
2018-10-14 22:07:08 +0200

    complete version
    complete version before deadline

--- js/functions.js
+++ js/functions.js
@@ -66,6 +66,8 @@ function personOnScreen(){
     let lat = parseFloat(person.coordinates.latitude);
     let lng = parseFloat(person.coordinates.longitude);
     getMap(lng, lat);
+    const distance = document.getElementsByClassName("mapboxgl-ctrl-bottom-right")[1];
+    distance.innerHTML = calculateDistance(lng, lat);
 };
 
 
@@ -130,4 +132,27 @@ function getMap(lng, lat){
     // disable zoom on map
     map.scrollZoom.disable();
     
+}
+
+
+function calculateDistance(lat1, lon1){
+    let lat2 = 0;
+    let lon2 = 0;
+    let unit = "K";
+    const userLocation = navigator.geolocation.watchPosition(function(position) {
+        console.log(position.coords.latitude, position.coords.longitude);
+        lon2 = position.coords.latitude;
+        lat2 = position.coords.longitude; 
+    });
+    let radlat1 = Math.PI * lat1/180;
+        let radlat2 = Math.PI * lat2/180;
+        let theta = lon1-lon2;
+        let radtheta = Math.PI * theta/180;
+        let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        dist = Math.acos(dist);
+        dist = dist * 180/Math.PI;
+        dist = dist * 60 * 1.1515;
+        if (unit=="K") { dist = dist * 1.609344 };
+        if (unit=="N") { dist = dist * 0.8684 };
+        return(dist.toFixed(0) + " km");
 }
